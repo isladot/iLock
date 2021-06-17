b048acce74b66f318823455231ba84110656a8ce
Luca Biagetti <notciabaku@gmail.com>
2021-06-17 21:08:54 +0200

    chore: code prettified.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import ctypes
 import platform
 import threading
@@ -11,34 +10,38 @@ load_dotenv()
 
 IP = os.environ.get("PHONE_IP")
 
+
 def ping_phone():
-  # Pings amount param.
-  param = '-n' if platform.system().lower() == 'windows' else '-c'
-  # Ping command.
-  command = ['ping', param, '1', IP]
-  # Pinging Phone. 
-  result = subprocess.run(command).returncode == 0
-  # Get and parse current time.
-  now = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+    # Pings amount param.
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    # Ping command.
+    command = ['ping', param, '1', IP]
+    # Pinging Phone.
+    result = subprocess.run(command).returncode == 0
+    # Get and parse current time.
+    now = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
 
-  # Check if Phone replied.
-  if(result):
-    # Log phone detection.
-    print(f'{now}: Phone detected.')
-  else:
-    # Log phone undetection.
-    print(f'{now}: Phone not detected, locking computer.')
-    # Lock computer.
-    lock_windows()
-    return
+    # Check if Phone replied.
+    if(result):
+        # Log phone detection.
+        print(f'{now}: Phone detected.')
+    else:
+        # Log phone undetection.
+        print(f'{now}: Phone not detected, locking computer.')
+        # Lock computer.
+        lock_windows()
+        return
 
-  # Periodically ping Phone.
-  threading.Timer(20, ping_phone).start()
+    # Periodically ping Phone.
+    threading.Timer(20, ping_phone).start()
+
 
 def lock_windows():
-  ctypes.windll.user32.LockWorkStation()
+    ctypes.windll.user32.LockWorkStation()
+
 
 def main():
-  ping_phone()
+    ping_phone()
+
 
 main()
